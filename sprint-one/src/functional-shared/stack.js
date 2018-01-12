c7122c5bc757d717dcb78daed027ded0722e8b93
Luis Ortiz <luis.ortiz1794@gmail.com>
2018-01-12 10:33:29 -0600

    worked on and finished second half of part 1 (queue, stacks)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+    var someInstance = {
+      count: 0,
+      storage: {}
+  }
+
+    _.extend(someInstance, stackMethods)
+    return someInstance;
+  };
+
+  var stackMethods = {
+
+   push:function(value) {
+      this.storage[this.count] = value;
+      this.count++;
+    },
+   pop:function() {
+      this.count--;
+      var counted = this.storage[this.count]
+      delete this.storage[this.count];
+      return counted;
+    },
+  size:function() {
+      if (this.count > 0) {
+        return this.count;
+      } else {
+        return 0;
+      }
+  }
 };
-
-var stackMethods = {};
-
-
